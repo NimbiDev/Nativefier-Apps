0c0661ca04ad1ae508f2df85b029dc8136751710
Nimbi <JoshuaVL1988@protonmail.com>
2022-02-05 00:42:15 -0700

    Update build.bat

--- YouTube/build.bat
+++ YouTube/build.bat
@@ -11,5 +11,5 @@ Echo.
 ECHO         ****************************************
 ECHO          Building native app... Please wait...
 ECHO         ****************************************
-nativefier -n "YouTube" "https://www.youtube.com/" -u firefox -e "15.3.5" --tray "true" --enable-es3-apis --widevine --verbose --background-color "#333333" --internal-urls "(.*?)(accounts\.google\.com/ServiceLogin|\.youtube\.com|youtu\.be)(.*?)" --file-download-options "{\"saveAs\": true}" --icon "bin\icon.ico" --global-shortcuts "bin\global-shortcuts.json" >> "%mydate%_%mytime%" 2>>& 1 && copy "bin\sign-pkg.cmd" "YouTube-win32-x64\sign-pkg.cmd" >> "%mydate%_%mytime%" 2>>& 1 && copy "bin\upgrade-pkg.cmd" "YouTube-win32-x64\upgrade-pkg.cmd" >> "%mydate%_%mytime%" 2>>& 1
+nativefier -n "YouTube" "https://www.youtube.com/" -u firefox -e "15.3.5" --tray "true" --enable-es3-apis --verbose --background-color "#333333" --internal-urls "(.*?)(accounts\.google\.com/ServiceLogin|\.youtube\.com|youtu\.be)(.*?)" --file-download-options "{\"saveAs\": true}" --icon "bin\icon.ico" --global-shortcuts "bin\global-shortcuts.json" >> "%mydate%_%mytime%" 2>>& 1 && copy "bin\sign-pkg.cmd" "YouTube-win32-x64\sign-pkg.cmd" >> "%mydate%_%mytime%" 2>>& 1 && copy "bin\upgrade-pkg.cmd" "YouTube-win32-x64\upgrade-pkg.cmd" >> "%mydate%_%mytime%" 2>>& 1
 exit /b
