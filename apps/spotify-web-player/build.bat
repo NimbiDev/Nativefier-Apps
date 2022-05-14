9a53e25d7e18392433f30c0d76f216835aa91721
Joshua Lewis <JoshuaVL1988@protonmail.com>
2022-05-13 17:13:04 -0700

    :recycle: 05/13/2022 - 05:12 PM

--- apps/spotify-web-player/build.bat
+++ apps/spotify-web-player/build.bat
@@ -16,7 +16,8 @@ For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (Set my_time=%%a-%%b)
 
 echo %my_date%_%my_time%
 
-Set debug_path="..\..\logs\%app_name%\%app_title%_%my_date%_%my_time%.log"Set app_path="..\..\apps\%app_name%\%app_name%-win32-x64"
+Set debug_path="..\..\logs\%app_name%\%app_title%_%my_date%_%my_time%.log"
+Set app_path="..\..\apps\%app_name%\%app_name%-win32-x64"
 
 CLS & COLOR 0A & echo.
 Mode con:cols=60 lines=5
